58e9f495b6f112ca968d2a1a636627c8b35a6936
Nayanpawar03 <nayanpawar136@gmail.com>
2025-10-02 16:53:08 +0530

    Created backend file structures

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,9 @@
 import { useState } from 'react'
 import { Routes, Route } from "react-router-dom"
-import Home from "../src/pages/Home"
-import Register from "../src/pages/Register"
-import Login from "../src/pages/Login"
-import Dashboard from "../src/pages/Dashboard"
+import Home from "./pages/Home"
+import Register from "./pages/Register"
+import Login from "./pages/Login"
+import Dashboard from "./pages/Dashboard"
 
 
 function App() {
