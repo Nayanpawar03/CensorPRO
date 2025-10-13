f6b0193119e30767276958723ae479f3d32af9f5
Nayanpawar03 <nayanpawar136@gmail.com>
2025-10-13 22:22:36 +0530

    Modified github workflow file and added new dependency

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import copy from 'rollup-plugin-copy'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    copy({
+      targets: [
+        { src: './staticwebapp.config.json', dest: 'dist' }
+      ],
+      hook: 'writeBundle'
+    })
+  ],
 })
